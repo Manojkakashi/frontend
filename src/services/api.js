f64318a77a81982a67c00904dec1168695720d3e
Manojkakashi <45232388+Manojkakashi@users.noreply.github.com>
2025-05-21 13:24:08 -0400

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -7,6 +7,7 @@ const api = axios.create({
 export const uploadFiles = files => {
   const form = new FormData();
   files.forEach(f => form.append('files', f));
-  // ← note the “/api” prefix here
+
+  // ✅ Note the leading "/api" here:
   return api.post('/api/upload', form);
 };
